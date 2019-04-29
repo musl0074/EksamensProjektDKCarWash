e165f33c4eaeef75ad641ae16a5c0f97e6ff8e6f
Muslim Al-Ali <musl0074@edu.eal.dk>
2019-04-29 13:57:49 +0200

    Adding classes and refactor

--- EksamensProjektDKCarWash/Application/BookingViewModel.cs
+++ EksamensProjektDKCarWash/Application/BookingViewModel.cs
@@ -1,33 +1,21 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using Domain;
 
 namespace Application
 {
-   public class BookingViewModel
+    public class BookingViewModel
     {
         private BookingRepo br = new BookingRepo();
         private DBViewModel dbvm = new DBViewModel();
 
-
-
-        public void CreateBooking(string customerName,DateTime startTime,string email, string telephone, Package package)
+        public void DeleteBooking(int bookingID)
         {
-         Booking b =  dbvm.CreatePrivateBooking(customerName, startTime, email, telephone, package);
-            try
-            {
-                br.CreateBooking(b);
-            }
-            catch (NullReferenceException e)
-            {
-                MessageBox.Show(e.Message);
-                throw;
-            }
-            
+
         }
+
     }
 }
