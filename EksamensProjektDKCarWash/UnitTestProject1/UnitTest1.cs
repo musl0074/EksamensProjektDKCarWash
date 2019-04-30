c54b8c42f9b67c724c7ca111f54d082d4745e80b
Frank Nordahl Andersen <fran2238@edu.eal.dk>
2019-04-30 10:40:13 +0200

    tilrettede test

--- EksamensProjektDKCarWash/UnitTestProject1/UnitTest1.cs
+++ EksamensProjektDKCarWash/UnitTestProject1/UnitTest1.cs
@@ -40,21 +40,42 @@ namespace UnitTestProject1
 
         [TestMethod]
         public void DeletedFromBookingRepo () // Testen bliver KUN grøn, hvis der KUN er ÉN booking inde i systemet, når testen bliver kørt
-        {   
-            b1 = dbvmT.Sp_CreatePrivateBooking("Frank", new DateTime(2019, 9, 22, 10, 00, 00), new DateTime(2019, 9, 22, 00, 00, 00), "frank@eal.dk", "+4511223344", p1);
-            brT.CreateBooking(b1);
+        {
+            //  b1 = brT.Sp_CreatePrivateBooking("Frank", new DateTime(2019, 9, 22, 10, 00, 00), new DateTime(2019, 9, 22, 00, 00, 00), "frank@eal.dk", "+4511223344", p1);
+            //b1.CustomerName = "Frank";
+            //b1.StartTime = new DateTime(2019, 9, 22, 10, 00, 00);
+            //b1.BookingDate = new DateTime(2019, 9, 24, 10, 00, 00);
+            //b1.Email = "frank@eal.dk";
+            //b1.Telephone = "+4522334455";
+            //b1.Package = p1;
+            //b1.Vat = "88888888";
+            //b1.Id = 2;
+            b1 = new Booking("Frank", new DateTime(2019, 9, 22, 10, 00, 00), new DateTime(2019, 9, 24, 10, 00, 00), "frank@eal.dk", "+4511223344", p1, 2, "");
+           Booking b2 = new Booking ("Frank", new DateTime(2019, 9, 22, 10, 00, 00), new DateTime(2019, 9, 24, 10, 00, 00), "frank@eal.dk", "+4511223344", p1, 4, "");
+           Booking b3 = new Booking("Frank", new DateTime(2019, 9, 22, 10, 00, 00), new DateTime(2019, 9, 24, 10, 00, 00), "frank@eal.dk", "+4511223344", p1, 3, "");
+           Booking b4 = new Booking("Frank", new DateTime(2019, 9, 22, 10, 00, 00), new DateTime(2019, 9, 24, 10, 00, 00), "frank@eal.dk", "+4511223344", p1, 5, "");
+            // brT.CreateBooking(b1);
             List<Booking> bookingsT = brT.GetBookings();
-            bvmT.DeleteBooking(b1.Id);
-
-            Booking b2 = null;
-            foreach (var item in bookingsT)
-            {
-                if (item.Id == b1.Id)
-                    b2 = item;
-            }
+            bookingsT.Add(b1);
+            bookingsT.Add(b2);
+            bookingsT.Add(b3);
+            bookingsT.Add(b4);
+            brT.DeleteBooking(b1.Id);
 
 
-            Assert.IsTrue(b2 == null); // Hvis b2 aldrig bliver sat, så er testen korrekt gennemført, da den ikke kunne finde den nylige oprettede booking
+            Assert.AreEqual(bookingsT.Count, 3);
+            
+          //  bvmT.DeleteBooking(b1.Id);
+
+            //Booking b2 = null;
+            //foreach (var item in bookingsT)
+            //{
+            //    if (item.Id == b1.Id)
+            //        b2 = item;
+            //}
+
+
+           // Assert.IsTrue(b2 == null); // Hvis b2 aldrig bliver sat, så er testen korrekt gennemført, da den ikke kunne finde den nylige oprettede booking
         }
 
     }
