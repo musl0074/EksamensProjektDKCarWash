4488e6058c03c4813bcaa47a13169b21a8968c1f
Muslim Al-Ali <musl0074@edu.eal.dk>
2019-05-16 14:34:45 +0200

    implementing listview wpf

--- EksamensProjektDKCarWash/UILayer/PickUpAgreementsPage.xaml.cs
+++ EksamensProjektDKCarWash/UILayer/PickUpAgreementsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,50 @@ namespace UILayer
     /// </summary>
     public partial class PickUpAgreementsPage : Page
     {
+        PickUpAgreementController puac = new PickUpAgreementController();
         public PickUpAgreementsPage()
         {
             InitializeComponent();
+            List<string> stringPickUpAgreements = puac.GetAllPickUpAgreements();
+            string[] pickupagreementsarray = new string[9];
+            
+            foreach (string pickupagreement in stringPickUpAgreements)
+            {
+
+               pickupagreementsarray = pickupagreement.Split(',');
+              
+            }
+           
+        }
+
+        private void UpdatePickUpAgreement_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeletePickUpAgreement_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchPickUpAgreement_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void hej_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
